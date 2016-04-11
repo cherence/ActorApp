cc72eae2a1f1f5c9f686179b1ef14ebb0c28f645
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-04-11 11:56:56 -0700

    removed creation of views from MainActivity

--- ActorApp/app/src/main/java/com/example/cher/actorapp/MainActivity.java
+++ ActorApp/app/src/main/java/com/example/cher/actorapp/MainActivity.java
@@ -2,20 +2,12 @@ package com.example.cher.actorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
-    ListView mainListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeViews();
-    }
-
-    private void initializeViews(){
-        mainListView = (ListView) findViewById(R.id.listView_id);
     }
 }
