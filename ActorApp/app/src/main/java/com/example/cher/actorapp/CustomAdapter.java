74dfb3113e52cb1532a2ec5239c7d071009f7412
adao1123 <adao1193@gmail.com>
2016-04-11 11:16:49 -0700

    Adapters and Actor class

---
+++ ActorApp/app/src/main/java/com/example/cher/actorapp/CustomAdapter.java
@@ -0,0 +1,34 @@
+package com.example.cher.actorapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by adao1 on 4/11/2016.
+ */
+public class CustomAdapter extends ArrayAdapter {
+    ArrayList<Actor> arrayList;
+
+    public CustomAdapter(Context context,ArrayList<Actor> actorArrayList) {
+        super(context, -1, actorArrayList);
+        arrayList = actorArrayList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        TextView name = (TextView)rowItem.findViewById(R.id.name_textView_id);
+        TextView dob = (TextView)rowItem.findViewById(R.id.dob_textView_id);
+        TextView oscars = (TextView)rowItem.findViewById(R.id.oscarsWon_textView_id);
+        name.setText(arrayList.get(position).getName());
+        dob.setText(arrayList.get(position).getDob());
+        oscars.setText(arrayList.get(position).getOscars());
+        return rowItem;
+    }
+}
