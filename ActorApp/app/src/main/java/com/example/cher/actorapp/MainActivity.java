e503918c0af4ed30586f0a1e39887fa072b54b2a
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-04-11 11:07:02 -0700

    created layout in activity_main.xml and list_item.xml and declared and initialized views in MainActivity.java

--- ActorApp/app/src/main/java/com/example/cher/actorapp/MainActivity.java
+++ ActorApp/app/src/main/java/com/example/cher/actorapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.cher.actorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mainListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
+    }
+
+    private void initializeViews(){
+        mainListView = (ListView) findViewById(R.id.listView_id);
     }
 }
